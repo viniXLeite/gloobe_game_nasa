dc8c8de8198a76ec19545bbe6e6ccee028b8bffa
Caiuã Gomes <69342553+Lentolan@users.noreply.github.com>
2024-10-05 22:39:08 -0300

    att inutil

--- jogo/scripts/npc.gd
+++ jogo/scripts/npc.gd
@@ -1,4 +1,10 @@
 extends CharacterBody2D
 
-func npc():
-	pass
+var speed = 6000
+
+func moving(delta):
+	velocity.x = speed * delta
+	move_and_slide()
+
+func _physics_process(delta):
+	moving(delta)
--- jogo/scripts/protag.gd
+++ jogo/scripts/protag.gd
@@ -1,17 +1,10 @@
 extends CharacterBody2D
 
-var npc_in_range = false
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder
 
-func _physics_process(delta):
-	if npc_in_range:
-		if Input.is_action_just_pressed("ui_accept"):
-			DialogueManager.show_dialogue_balloon(load("res://scripts/Gwenpool.dialogue"), "start")
-			return
-
-func _on_detection_area_body_entered(body):
-	if body.has_method("npc"):
-		npc_in_range = true
-		
-func _on_detection_area_body_exited(body):
-	if body.has_method("npc"):
-		npc_in_range = false
+func _trigger(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+		return
