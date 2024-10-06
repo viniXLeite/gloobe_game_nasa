2a27e73f149c7914ce4b634bbe5f93f0b8564ccf
Caiuã Gomes <69342553+Lentolan@users.noreply.github.com>
2024-10-06 18:56:36 -0300

    att

--- jogo/scripts/npc.gd
+++ jogo/scripts/npc.gd
@@ -1,18 +1,15 @@
 extends CharacterBody2D
 
-@onready var actionable_finder: Area2D = $ActionableFinder
+var speed = 200
 
-var speed = Vector2(200, 0)
-
-#signal collision_signal
+func _ready():
+	velocity.x = 200
 
 func _physics_process(delta):
-	
-	var collision = move_and_collide(speed * delta)
+	var collision = move_and_collide(velocity * delta)
 	
 	if collision:
-		speed = Vector2()
+		velocity.x = 0
 		DialogueManager.show_example_dialogue_balloon(load("res://scripts/Gwenpool.dialogue"), "start")
-		
-#func _on_collision_signal():
-#	print("Collision detected! Triggering response.")
+	if Input.is_action_just_pressed("ui_right"):
+		velocity.x = -200
