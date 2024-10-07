2e8e2b01456b3abfc12e3792683c9ed68fe71f1f
Caiuã Gomes <69342553+Lentolan@users.noreply.github.com>
2024-10-06 22:47:56 -0300

    att

--- jogo/scripts/npc.gd
+++ jogo/scripts/npc.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-var speed = 200
-
 func _ready():
-	velocity.x = 200
+	pass
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
@@ -11,5 +9,9 @@ func _physics_process(delta):
 	if collision:
 		velocity.x = 0
 		DialogueManager.show_example_dialogue_balloon(load("res://scripts/Gwenpool.dialogue"), "startTutorial")
-	if Input.is_action_just_pressed("ui_right"):
-		velocity.x = -200
+
+func _process(delta):
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.x = -200
+		if Input.is_action_just_pressed("ui_right"):
+			velocity.x = 200
